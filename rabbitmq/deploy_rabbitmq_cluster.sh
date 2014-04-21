0fbd50f9ea3945a1bfc3028cb13c3c6db907c8bc
root <root@mq-1.localdomain>
2014-04-21 14:18:34 +0800

    1st test

--- rabbitmq/deploy_rabbitmq_cluster.sh
+++ rabbitmq/deploy_rabbitmq_cluster.sh
@@ -3,8 +3,18 @@
 # Author dysj4099@gmail.com
 # April 15, 2014
 
+# Debug
+set -x
+
 # Read config
 . ./deploy_rabbitmq_cluster.cfg
+
+# Set /etc/hosts
+touch /tmp/tmp_hosts
+for i in ${!NODES[@]}; do
+    eval node_info=(\${${NODES[i]}[@]})
+    echo "${node_info[1]}	${node_info[0]}" >> /tmp/tmp_hosts
+done
 
 # Get MY_IP
 if [ "${MY_IP}" == "" ];then
@@ -40,15 +50,16 @@ while getopts 'h:r' OPT; do
         h)
 	    HOSTNAME_ROOT="$OPTARG";;
 	r)
-	    RAM_MODE=true;;												   esac
+	    RAM_MODE=true;;												   
+    esac
 done
 # Remove old version
-apt-get -y purge rabbitmq-server
+apt-get -y --force-yes purge rabbitmq-server
 # Install new version
 echo "deb http://www.rabbitmq.com/debian/ testing main" >> /etc/apt/source.list
-sudo apt-key add rabbitmq-signing-key-public.asc
+sudo apt-key add /tmp/rabbitmq-signing-key-public.asc
 apt-get update
-apt-get -y install rabbitmq-server
+apt-get -y --force-yes install rabbitmq-server
 # Stop service
 service rabbitmq-server stop
 # Set erlang cookie
@@ -67,9 +78,37 @@ else
     /usr/sbin/rabbitmqctl join_cluster rabbit@${HOSTNAME_ROOT}
 fi
 /usr/sbin/rabbitmqctl start_app
+service rabbitmq-server restart
 _wrtend_
 
+ROOT_HOST=""
 for i in ${!NODES[@]}; do
     #idx=`expr $i + 1`
+    eval node_info=(\${${NODES[i]}[@]})
+    #echo ${node_info[0]}
+    #echo ${node_info[1]}
+    #echo ${node_info[2]}
+    if [ i -eq 0 ];then
+        ROOT_HOST=${node_info[1]}
+    fi
 
+    sshpass -p ${ROOT_PASS} scp -o StrictHostKeyChecking=no /tmp/tmp_install_rabbitmq.sh root@${node_info[1]}:/tmp/
+    sshpass -p ${ROOT_PASS} scp -o StrictHostKeyChecking=no /tmp/tmp_hosts root@${node_info[1]}:/tmp/
+    sshpass -p ${ROOT_PASS} scp -o StrictHostKeyChecking=no ./rabbitmq-signing-key-public.asc root@${node_info[1]}:/tmp/
+    sshpass -p ${ROOT_PASS} ssh -o StrictHostKeyChecking=no root@${node_info[1]} cat /tmp/tmp_hosts >> /etc/hosts
+
+    if [ "${node_info[2]}" == "ram" ];then
+        sshpass -p ${ROOT_PASS} ssh -o StrictHostKeyChecking=no root@${node_info[1]} /bin/bash /tmp/tmp_install_rabbitmq.sh -h ${ROOT_HOST} -r
+    else
+        sshpass -p ${ROOT_PASS} ssh -o StrictHostKeyChecking=no root@${node_info[1]} /bin/bash /tmp/tmp_install_rabbitmq.sh -h ${ROOT_HOST}
+    fi
+done
+
+for i in ${USERS[@]}; do
+    eval user_info=(\${${USERS[i]}[@]})
+    #echo ${user_info[0]}
+    #echo ${user_info[1]}
+    /usr/sbin/rabbitmqctl add_user ${user_info[0]} ${user_info[1]}
+    /usr/sbin/rabbitmqctl set_user_tags ${user_info[0]} ${user_info[2]}
+    /usr/sbin/rabbitmqctl ser_permissions -p / ${user_info[0]} ".*" ".*" ".*"
 done
