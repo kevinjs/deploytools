547cccf17c4d47d443a046564955da0dc822146c
kevinjs <dysj4099@gmail.com>
2015-07-28 21:20:49 +0800

    add async batch

---
+++ async_batch/async_batch.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+#coding=utf-8
+#Author: calvinshao
+import sys
+import os
+import re
+import getopt
+import time
+import csv
+from functools import wraps
+
+def timeit(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        st = time.time()
+        ret = func(*args, **kwargs)
+        et = time.time()
+        print 'cost: %s sec' %(et-st)
+        return ret
+    return wrapper
+
+def exe_by_expect():
+    pass
+
+def exe_by_sshpass():
+    pass
+
+def read_hosts(hosts_file):
+    hosts = []
+    rex_ip_p = re.compile(r'((([1-9]?|1\d)\d|2([0-4]\d|5[0-5]))\.){3}(([1-9]?|1\d)\d|2([0-4]\d|5[0-5]))(:[0-9]+)?')
+    if hosts_file and os.path.isfile(hosts_file):
+        with open(hosts_file, 'r') as f:
+            try:
+                for ip_port, acc, pwd in csv.reader(f):
+                    tmp_svr_info = {}
+                    if re.search(rex_ip_p, ip_port):
+                        if ':' in ip_port:
+                            tmp_svr_info['ip'] = ip_port.split(':')[0]
+                            tmp_svr_info['port'] = int(ip_port.split(':')[1])
+                        else:
+                            tmp_svr_info['ip'] = ip_port.split(':')[0]
+                            tmp_svr_info['port'] = 22
+                        tmp_svr_info['acc'] = acc
+                        tmp_svr_info['pwd'] = pwd
+                        hosts.append(tmp_svr_info)
+            except ValueError:
+                print 'ERROR:hosts file format'
+    return hosts
+
+@timeit
+def exe_batch(mode, remote_hosts_file, script_file):
+    hosts = read_hosts(remote_hosts_file)
+
+if __name__=='__main__':
+    remote_hosts_file = ''
+    mode = 'expect'
+    script_file = ''
+
+    opts, args = getopt.getopt(sys.argv[1:], "m:r:s:h")
+    for op, value in opts:
+        if op == "-m":
+            mode = value
+        elif op == "-r":
+            remote_hosts_file = value
+        elif op == "-s":
+            script_file = value
+        elif op == "-h":
+            print 'USAGE: python %s -m mode -r remote_host_file -s script_file' %sys.argv[0]
+
+    if mode in ['expect', 'sshpass'] \
+       and remote_hosts_file \
+       and script_file:
+        exe_batch(mode, remote_hosts_file, script_file)
+    else:
+        print 'USAGE: python %s -m mode -r remote_host_file -s script_file' %sys.argv[0]
+        sys.exit(-1)
+
